b69a7ab9f90d9ed01c905b32c604e238e20e1807
madnan610 <106472851+madnan610@users.noreply.github.com>
2022-06-08 08:55:39 -0700

    braclaycapitals

---
+++ app/Http/Controllers/Admin/PaymentLogController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Fund;
+use Facades\App\Services\BasicService;
+use Illuminate\Http\Request;
+
+class PaymentLogController extends Controller
+{
+    public function index()
+    {
+        $page_title = "Payment Logs";
+
+        $funds = Fund::where('status', '!=', 0)->where('plan_id',null)->orderBy('id', 'DESC')->with('user', 'gateway')->paginate(config('basic.paginate'));
+        return view('admin.payment.logs', compact('funds', 'page_title'));
+    }
+
+    public function search(Request $request)
+    {
+        $search = $request->all();
+
+        $dateSearch = $request->date_time;
+        $date = preg_match("/^[0-9]{2,4}\-[0-9]{1,2}\-[0-9]{1,2}$/", $dateSearch);
+
+        $funds = Fund::when(isset($search['name']), function ($query) use ($search) {
+
+                return $query->where('transaction', 'LIKE', $search['name'])
+                    ->orWhereHas('user', function ($q) use ($search) {
+                        $q->where('email', 'LIKE', "%{$search['name']}%")
+                            ->orWhere('username', 'LIKE', "%{$search['name']}%");
+                    });
+            })
+            ->when($date == 1, function ($query) use ($dateSearch) {
+                return $query->whereDate("created_at", $dateSearch);
+            })
+            ->when($search['status'] != -1, function ($query) use ($search) {
+                return $query->where('status', $search['status']);
+            })
+            ->where('status', '!=', 0)->where('plan_id',null)
+            ->with('user', 'gateway')
+            ->paginate(config('basic.paginate'));
+        $funds->appends($search);
+        $page_title = "Search Payment Logs";
+        return view('admin.payment.logs', compact('funds', 'page_title'));
+    }
+}
